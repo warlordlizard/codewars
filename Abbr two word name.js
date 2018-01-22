d8464451056f7bb4348052610f3a631b9cd0034a
warlordlizard <33190146+warlordlizard@users.noreply.github.com>
2018-01-22 15:06:03 -0800

    Delete Abbr two word name.js

--- Abbr two word name.js
+++
@@ -1,9 +0,0 @@
-function abbrevName(name) {
-    var abbName = name.split(" ");
-    var first = abbName[0].charAt(0).toUpperCase();
-    var last = abbName[1].charAt(0).toUpperCase();
-
-    return first + "." + last;
-
-
-}
